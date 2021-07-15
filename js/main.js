5e5cdab98eb69d59a01f5541579207c1c8bc902b
husqy06 <luigf306@gmail.com>
2021-07-15 20:52:51 +0200

    Completamento esercizio

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+var nome = prompt("Scrivi il tuo nome");
+
+var cognome = prompt("Scrivi il tuo cognome");
+
+var colore = prompt("Scrivi il tuo colore preferito"); 
+
+alert("Password sicura:" + nome + cognome + colore + "21");
